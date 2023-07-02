55f4109773fc8b0840fec9cc8292bfa6ba4c3632
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2023-07-02 13:32:22 +0200

    initial commit

---
+++ src/main/java/com/ua/mytestbot/SpringTestBot/config/BotConfig.java
@@ -0,0 +1,18 @@
+package com.ua.mytestbot.SpringTestBot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("application.properties")
+@Data
+public class BotConfig {
+
+    @Value("${bot.name}")
+    String name;
+
+    @Value("${bot.token}")
+    String token;
+}
---
+++ src/main/java/com/ua/mytestbot/SpringTestBot/service/TelegramBot.java
@@ -0,0 +1,60 @@
+package com.ua.mytestbot.SpringTestBot.service;
+
+import com.ua.mytestbot.SpringTestBot.config.BotConfig;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@AllArgsConstructor
+public class TelegramBot extends TelegramLongPollingBot {
+    private final BotConfig botConfig;
+
+    @Override
+    public String getBotUsername() {
+        return botConfig.getName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfig.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+
+                default:
+                    String answer = "Sorry, command wasn't recognized!";
+                    sendMessage(chatId, answer);
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Hello, " + name + " How can i help you?";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage sendMessageField = new SendMessage();
+        sendMessageField.setChatId(String.valueOf(chatId));
+        sendMessageField.setText(textToSend);
+
+        try {
+            execute(sendMessageField);
+        } catch (TelegramApiException e) {
+            e.getStackTrace();
+        }
+    }
+}
